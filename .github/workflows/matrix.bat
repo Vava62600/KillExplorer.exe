ecafb4085a6d6972aad192b3c452722bb45c4755
Vava62600 <120785753+Vava62600@users.noreply.github.com>
2022-12-19 11:39:13 +0100

    Delete matrix.bat

--- .github/workflows/matrix.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-color 0A
-:matrix
-title The Matrix %random% %random% %random% %random% %random% %random% %random% %random% %random%
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto matrix
